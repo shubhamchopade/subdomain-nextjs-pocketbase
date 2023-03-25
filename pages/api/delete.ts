922b092b5caff1b979fc059dad22f6ac334bf873
shubham <shubhamchopade10@gmail.com>
2023-03-25 03:03:14 -0400

    clean

--- pages/api/delete.ts
+++ pages/api/delete.ts
@@ -22,41 +22,19 @@ export default function handler(
 
     const pb = new PocketBase("https://pocketbase.techsapien.dev");
 
-    // Add a promise.all() to execute all the commands at once
-
     // delete from pocketbase
     const deleteProjectPocketbase = pb.collection("projects").delete(projectId);
 
     // DELETE config file at nginx
     const deleteNginxConfig = executeCommandChild('rm', ['-f', `/etc/nginx/techsapien.d/${subdomain}.techsapien.dev.conf`])
-    // .then((res: any) => {
-    //     log(chalk.bgGreen(`DELETED ${subdomain}.techsapien.dev.conf`, res.stdout));
-    // }).catch(e => {
-    //     console.error("DELETING nginx/chsapien.d failed", e)
-    // })
 
     // delete the project files from /app 
     const deleteProjectFiles = executeCommandChild('rm', ['-rf', projectPath])
-    // .then((output: any) => {
-    //     log(chalk.bgBlue(`deletes the project files from /app SUCCESS`));
-    //     res.status(200).json({ data: "deleted project files" });
-    // }).catch(err => {
-    //     console.error("delete the project files from /app FAILED", err)
-    //     res.status(400).json({ data: "delete error" });
-    // })
 
     // disable the project from systemctl
     const disableProjectSystem = executeCommandChild(`systemctl`, [`disable`, `$(systemd-escape`, `--template`, `techsapien@.service`, `"${projectId} ${port} ${id} ${framework}")`])
-    // ).then((output: any) => {
-    //     console.log("Service disabled", output.stdout, output.stderr)
-    //     res.status(200).json({ data: "service disabled" });
-    // }).catch((err) => {
-    //     console.log("Service disabled failed", err);
-    //     res.status(400).json({ data: "service disabled failed" });
-    // }
-    // );
 
-    // Add a Promise.all() to execute all the commands at once
+    // Promise.all() to execute all the commands at once, in parallel
     Promise.all([
         deleteNginxConfig,
         deleteProjectFiles,
