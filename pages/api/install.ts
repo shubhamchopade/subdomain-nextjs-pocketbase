a97b15d258739c7755302b5733b6566e66252f66
shubham <shubhamchopade10@gmail.com>
2023-03-26 09:03:27 -0400

    moved pocketbase status to server

--- pages/api/install.ts
+++ pages/api/install.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import chalk from "chalk";
 import { executeCommandChild } from "../../backend/node-multithreading";
-
+import PocketBase from 'pocketbase'
 
 const log = console.log;
 const erB = chalk.bold.redBright;
@@ -11,22 +11,33 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const { link, id = 1, projectId = 1, port = 3 } = req.query;
+  const { link, id = 1, projectId = 1, port = 3, statusId } = req.query;
   const dir = "/home/shubham/Code/monorepo/apps";
+  const pb = new PocketBase("https://pocketbase.techsapien.dev");
 
   // get cwd
-  executeCommandChild(`cd`, [`${dir}/${id}/${projectId}`, `&&`, `pwd`]).then((output: any) => {
-    log(chalk.green("pwd >> ", output.stdout, output.stderr));
-  });
+  // executeCommandChild(`cd`, [`${dir}/${id}/${projectId}`, `&&`, `pwd`]).then((output: any) => {
+  //   log(chalk.green("pwd >> ", output.stdout, output.stderr));
+  // });
 
   // Install dependencies using npm
   executeCommandChild('cd', [`${dir}/${id}/${projectId}`, `&&`, `yarn install`])
     .then((output: any) => {
       log(chalk.bgMagenta("npm installed >> ", output.stdout, output.stderr));
+      pb.collection('projectStatus').update(statusId, {
+        install: true,
+        current: "installation complete",
+        logInstall: JSON.stringify(output.stdout)
+      })
       res.status(200).json({ data: "Installation Complete", logs: JSON.stringify(output.stdout) });
     })
     .catch((err) => {
       log(erB("--------npm install failed---------", err.stderr));
+      pb.collection('projectStatus').update(statusId, {
+        install: false,
+        current: "installation failed",
+        logInstall: JSON.stringify(err.stderr)
+      })
       res.status(400).json({ data: "Installation Failed", logs: JSON.stringify(err.stderr) });
     });
 }
