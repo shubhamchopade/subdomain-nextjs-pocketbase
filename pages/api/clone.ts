e9a6dd75acef4b48a3e22be0a60df307315663ff
shubham <shubhamchopade10@gmail.com>
2023-03-26 18:19:10 -0400

    moved framework logic to server

--- pages/api/clone.ts
+++ pages/api/clone.ts
@@ -6,25 +6,45 @@ import PocketBase from 'pocketbase'
 const log = console.log;
 const erB = chalk.bold.redBright;
 
+/**
+ * Clone api function first clones the repo and then detects the framework then returns
+ * @param req link, id, projectId, port, statusId
+ * @param res if success, update status to cloned
+ */
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   const { link, id = 1, projectId = 1, port = 3, statusId } = req.query;
   const dir = "/home/shubham/Code/monorepo/apps";
+  const scriptLocation = '/home/shubham/Code/nginx-config/get-framework.sh'
   const pb = new PocketBase("https://pocketbase.techsapien.dev");
-  // TODO: Add get framework api here
+  // TODO: Add get framework api
 
   console.log("clone started")
   try {
+
     executeCommandChild(`git`, ['clone', `${link}`, `${dir}/${id}/${projectId}`]).then(
       (childRes: any) => {
-        log(chalk.bgGreen(`Repo cloned - ${link}`, childRes.stderr));
+        log(chalk.bgGreen(`Repo cloned - ${link}`, childRes.stdout, childRes.stderr));
         pb.collection('projectStatus').update(statusId, {
           current: "cloned",
           cloned: true,
           logClone: "cloned successfully"
         })
+
+        // Create nginx file using custom script
+        executeCommandChild(
+          `sh`, [`${scriptLocation}`, `${dir}/${id}/${projectId}`]
+        ).then((output: any) => {
+          log(chalk.bgGreenBright.black("framework detected ", output.stdout, output.stderr));
+          // res.status(200).json({ data: output.stdout });
+        }).catch(e => {
+          log(chalk.redBright("unsupported framework detected", e));
+          // res.status(404).json({ data: "unsupported framework detected. We are still working on this, sorry." });
+        })
+
+
         res.status(200).json({ data: `Repo cloned ${link}`, logs: JSON.stringify(childRes.stdout), });
       }
     ).catch((err) => {
