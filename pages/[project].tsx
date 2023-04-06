5a27d5dc01b69681525bb405f1cb242a7db1d3cb
shubham <shubhamchopade10@gmail.com>
2023-04-06 02:59:29 -0400

    add rebuild project

--- pages/[project].tsx
+++ pages/[project].tsx
@@ -114,7 +114,27 @@ const Project = (props) => {
         }
     }
 
-    console.log(liveStatus)
+    // RE BUILD
+    const rebuild = async () => {
+        setIsLoading(true)
+        try {
+            const build = await buildDependencies()
+            setIsLoading(false)
+
+            if (build.status == 400) {
+                toast.error(`Build failed, please check the logs for more info`)
+                return
+            }
+
+            toast.success(`Project built successfully`)
+        } catch (e) {
+            setIsLoading(false)
+            toast.error(`Build failed, please check the logs for more info`)
+            console.log(e)
+        }
+    }
+
+    // console.log(liveStatus)
     return (
         <div className='mb-32 relative container mx-auto'>
             <div className='breadcrumbs'>
@@ -124,12 +144,20 @@ const Project = (props) => {
                 </ul>
             </div>
             <div className={`card bg-base-400 shadow-xl relative m-16 ${isLoading && "card-project"}`}>
-                <span
-                    onClick={handleDelete}
-                    className="absolute top-2 right-2 btn btn-xs btn-square btn-outline btn-error"
-                >
-                    x
-                </span>
+
+
+                <div className="dropdown dropdown-bottom dropdown-end absolute top-2 right-2 ">
+                    <label tabIndex={0} className="btn btn-square btn-ghost"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path className='fill-base-content' d="M12 15.5C11.0717 15.5 10.1815 15.1313 9.52509 14.4749C8.86871 13.8185 8.49996 12.9283 8.49996 12C8.49996 11.0717 8.86871 10.1815 9.52509 9.52513C10.1815 8.86875 11.0717 8.5 12 8.5C12.9282 8.5 13.8185 8.86875 14.4748 9.52513C15.1312 10.1815 15.5 11.0717 15.5 12C15.5 12.9283 15.1312 13.8185 14.4748 14.4749C13.8185 15.1313 12.9282 15.5 12 15.5ZM19.43 12.97C19.47 12.65 19.5 12.33 19.5 12C19.5 11.67 19.47 11.34 19.43 11L21.54 9.37C21.73 9.22 21.78 8.95 21.66 8.73L19.66 5.27C19.54 5.05 19.27 4.96 19.05 5.05L16.56 6.05C16.04 5.66 15.5 5.32 14.87 5.07L14.5 2.42C14.4796 2.30222 14.4183 2.19543 14.3268 2.11855C14.2353 2.04168 14.1195 1.99968 14 2H9.99996C9.74996 2 9.53996 2.18 9.49996 2.42L9.12996 5.07C8.49996 5.32 7.95996 5.66 7.43996 6.05L4.94996 5.05C4.72996 4.96 4.45996 5.05 4.33996 5.27L2.33996 8.73C2.20996 8.95 2.26996 9.22 2.45996 9.37L4.56996 11C4.52996 11.34 4.49996 11.67 4.49996 12C4.49996 12.33 4.52996 12.65 4.56996 12.97L2.45996 14.63C2.26996 14.78 2.20996 15.05 2.33996 15.27L4.33996 18.73C4.45996 18.95 4.72996 19.03 4.94996 18.95L7.43996 17.94C7.95996 18.34 8.49996 18.68 9.12996 18.93L9.49996 21.58C9.53996 21.82 9.74996 22 9.99996 22H14C14.25 22 14.46 21.82 14.5 21.58L14.87 18.93C15.5 18.67 16.04 18.34 16.56 17.94L19.05 18.95C19.27 19.03 19.54 18.95 19.66 18.73L21.66 15.27C21.78 15.05 21.73 14.78 21.54 14.63L19.43 12.97Z" />
+                    </svg>
+                    </label>
+                    <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-52">
+                        <li onClick={() => router.push(`/create/secrets?projectId=${projectId}&statusId=${status.id}&name=${title}&id=${id}&metricId=${projectMetrics.id}`)
+                        }><a>Edit Secrets - .env</a></li>
+                        <li onClick={handleDelete}><a className='btn-outline btn-error'>Delete</a></li>
+                    </ul>
+                </div>
+
                 <span className="uppercase text-xs font-bold">{framework}</span>
                 <div className="card-body">
                     {data?.id && <Link href={`${projectId}`} className="card-title">{title}</Link>}
@@ -149,6 +177,9 @@ const Project = (props) => {
                         <button onClick={deploy} className={`btn btn-primary text-xs btn-xs ${status?.isOnline && "hidden"} ${isLoading && "loading disabled"}`}>
                             DEPLOY
                         </button>
+                        {status?.isOnline && <button onClick={rebuild} className={`btn btn-primary text-xs btn-xs ${isLoading && "loading disabled"}`}>
+                            RE-BUILD
+                        </button>}
                     </div>
                 </div>
             </div>
