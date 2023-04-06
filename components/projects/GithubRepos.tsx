472c302e5ecefb5b6322fdd3b7b7f34a257ae078
shubham <shubhamchopade10@gmail.com>
2023-04-06 01:14:57 -0400

    restrict repos greater than 50 mb

--- components/projects/GithubRepos.tsx
+++ components/projects/GithubRepos.tsx
@@ -20,6 +20,7 @@ const GithubRepos = () => {
 
         if (username) {
             const reposRes = await getRepos(username)
+            console.log(reposRes)
             setRepos(reposRes)
         }
 
@@ -39,24 +40,32 @@ const GithubRepos = () => {
 
     return (
         <div className='mx-auto w-96 grid place-items-center prose mt-4 mb-8'>
-            <div className='transform scale-50'>
-                <svg width="204" height="204" viewBox="0 0 204 204" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="102" cy="102" r="102" fill="white" />
-                    <path d="M101.632 41.3993C93.7222 41.3993 85.8897 42.9573 78.5818 45.9843C71.2739 49.0114 64.6337 53.4481 59.0405 59.0414C47.7445 70.3374 41.3984 85.6581 41.3984 101.633C41.3984 128.256 58.6855 150.844 82.5984 158.855C85.6101 159.337 86.5738 157.47 86.5738 155.844V145.664C69.889 149.278 66.3352 137.593 66.3352 137.593C63.5645 130.606 59.6493 128.738 59.6493 128.738C54.168 125.004 60.0709 125.124 60.0709 125.124C66.0943 125.546 69.2867 131.328 69.2867 131.328C74.527 140.484 83.3814 137.773 86.8147 136.328C87.3568 132.413 88.9229 129.762 90.6095 128.256C77.2376 126.751 63.2031 121.57 63.2031 98.6214C63.2031 91.9355 65.492 86.5747 69.4072 82.2981C68.8048 80.7922 66.6966 74.5279 70.0095 66.3963C70.0095 66.3963 75.0691 64.77 86.5738 72.5402C91.3323 71.215 96.5124 70.5525 101.632 70.5525C106.752 70.5525 111.932 71.215 116.691 72.5402C128.195 64.77 133.255 66.3963 133.255 66.3963C136.568 74.5279 134.46 80.7922 133.857 82.2981C137.773 86.5747 140.061 91.9355 140.061 98.6214C140.061 121.631 125.967 126.69 112.535 128.196C114.703 130.063 116.691 133.738 116.691 139.339V155.844C116.691 157.47 117.654 159.397 120.726 158.855C144.639 150.784 161.866 128.256 161.866 101.633C161.866 93.7231 160.308 85.8905 157.281 78.5826C154.254 71.2747 149.817 64.6346 144.224 59.0414C138.631 53.4481 131.991 49.0114 124.683 45.9843C117.375 42.9573 109.542 41.3993 101.632 41.3993Z" fill="black" />
+
+            <div className='bg-white rounded-full'>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path className='fill-black' d="M12 2C10.6868 2 9.38642 2.25866 8.17317 2.7612C6.95991 3.26375 5.85752 4.00035 4.92893 4.92893C3.05357 6.8043 2 9.34784 2 12C2 16.42 4.87 20.17 8.84 21.5C9.34 21.58 9.5 21.27 9.5 21V19.31C6.73 19.91 6.14 17.97 6.14 17.97C5.68 16.81 5.03 16.5 5.03 16.5C4.12 15.88 5.1 15.9 5.1 15.9C6.1 15.97 6.63 16.93 6.63 16.93C7.5 18.45 8.97 18 9.54 17.76C9.63 17.11 9.89 16.67 10.17 16.42C7.95 16.17 5.62 15.31 5.62 11.5C5.62 10.39 6 9.5 6.65 8.79C6.55 8.54 6.2 7.5 6.75 6.15C6.75 6.15 7.59 5.88 9.5 7.17C10.29 6.95 11.15 6.84 12 6.84C12.85 6.84 13.71 6.95 14.5 7.17C16.41 5.88 17.25 6.15 17.25 6.15C17.8 7.5 17.45 8.54 17.35 8.79C18 9.5 18.38 10.39 18.38 11.5C18.38 15.32 16.04 16.16 13.81 16.41C14.17 16.72 14.5 17.33 14.5 18.26V21C14.5 21.27 14.66 21.59 15.17 21.5C19.14 20.16 22 16.42 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7362 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2Z" />
                 </svg>
             </div>
 
-            <h1 className='text-center my-2'>These are your github public repositories. Please select the next app you want to import to reactly!</h1>
-            <div className=''>
+            <h1 className='my-2'>These are your github public repositories. Please select the next app you want to import to reactly!</h1>
+            <p className='my-2'>Please note - repositories greater than 50 MB are not yet supported.</p>
+
+            {/* <div className=''>
                 <label className='text-xs' htmlFor='project-search'>Search github repository</label>
                 <input className='input input-bordered bg-base-300 w-full' name="project-search" value={"input"} onChange={() => console.log("clicked")} />
-                {/* <button onClick={() => handleCreateProject("add", "")}>Import</button> */}
-            </div>
+            </div> */}
+
+
             <div className="flex flex-col h-96 overflow-y-auto overflow-x-hidden">
                 {
                     repos.map(repo => (
-                        <div key={repo.id} className='cursor-pointer hover:ring card bg-base-300 my-2 mx-auto w-11/12' >
-                            <LinkCard name={repo.name} link={repo.html_url} />
+                        <div key={repo.id} className='my-2 mx-auto w-11/12' >
+                            <LinkCard
+                                name={repo.name}
+                                link={repo.html_url}
+                                size={repo.size}
+                                stars={repo.stargazers_count}
+                                forks={repo.forks_count} />
                         </div>))
                 }
             </div>
--- components/projects/LinkCard.tsx
+++ components/projects/LinkCard.tsx
@@ -7,9 +7,7 @@ import { useRouter } from 'next/router';
 const LinkCard = (props) => {
     const [repos, setRepos] = useState([])
     const router = useRouter()
-
-    const name = props?.name
-    const link = props?.link
+    const { name, link, size, stars, forks } = props
 
     const handleCreateProject = (name, link) => {
         const auth = localStorage.getItem("pocketbase_auth")
@@ -68,14 +66,44 @@ const LinkCard = (props) => {
         createProject();
     };
 
+    const sizeMb = (size / 1024) * 2
+    const sizeGreaterThan50 = sizeMb > 50
+
     return (
-        <div className='card shadow-md '>
-            <div className='flex justify-between'>
-                <p className='text-md font-bold ml-4 my-4'>{name}</p>
-                <button onClick={() => handleCreateProject(name, link)} className='btn btn-xs m-2'>import</button>
+        <div className={`card bg-base-300 shadow-md py-2 ${sizeMb > 50 ? 'cursor-not-allowed' : 'hover:ring-2'} `}>
+            <div className='flex justify-between items-center'>
+                <div>
+                    <p className='text-md ml-4'>{name}</p>
+                    <div className='flex items-center ml-4 mt-2'>
+                        <div className='flex justify-center items-center text-xs'>
+                            <StarIcon /> <span>{stars}</span>
+                        </div>
+
+                        <div className='flex justify-center items-center text-xs ml-2'>
+                            <ForkIcon /> <span>{forks}</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='flex flex-col items-center mr-2'>
+                    <button onClick={() => handleCreateProject(name, link)} disabled={sizeMb > 50} className={`btn btn-xs mt-2 mr-2`}>import</button>
+                    <span className='text-xs'>{sizeMb.toFixed(2)} MB</span>
+                </div>
             </div>
         </div>
     )
 }
 
 export default LinkCard
+
+const StarIcon = () => <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path className='fill-base-content' d="M7.49995 10.7938L10.0937 12.3625C10.5687 12.65 11.15 12.225 11.025 11.6875L10.3375 8.73751L12.6312 6.75001C13.05 6.38751 12.825 5.70001 12.275 5.65626L9.2562 5.40001L8.07495 2.61251C7.86245 2.10626 7.13745 2.10626 6.92495 2.61251L5.7437 5.39376L2.72495 5.65001C2.17495 5.69376 1.94995 6.38126 2.3687 6.74376L4.66245 8.73126L3.97495 11.6813C3.84995 12.2188 4.4312 12.6438 4.9062 12.3563L7.49995 10.7938Z" />
+</svg>
+
+
+const ForkIcon = () => <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path className='fill-base-content' d="M13.75 3.12498C13.7514 2.78091 13.6581 2.44308 13.4804 2.14849C13.3026 1.85389 13.0472 1.61389 12.7421 1.45477C12.4371 1.29565 12.0941 1.22354 11.7508 1.24635C11.4074 1.26915 11.077 1.38598 10.7957 1.58405C10.5143 1.78212 10.2929 2.05379 10.1556 2.36931C10.0184 2.68482 9.97062 3.03202 10.0175 3.37289C10.0644 3.71375 10.2043 4.03513 10.4216 4.30184C10.639 4.56854 10.9256 4.77029 11.25 4.88499V6.87499H3.75V4.88499C4.16701 4.73755 4.51848 4.44744 4.74227 4.06594C4.96607 3.68443 5.0478 3.23609 4.973 2.80015C4.89821 2.36422 4.67171 1.96876 4.33354 1.68367C3.99537 1.39858 3.56731 1.24222 3.125 1.24222C2.6827 1.24222 2.25463 1.39858 1.91647 1.68367C1.5783 1.96876 1.3518 2.36422 1.27701 2.80015C1.20221 3.23609 1.28394 3.68443 1.50773 4.06594C1.73153 4.44744 2.083 4.73755 2.5 4.88499V6.87499C2.5 7.20651 2.6317 7.52445 2.86612 7.75887C3.10054 7.99329 3.41848 8.12499 3.75 8.12499H6.875V10.74C6.458 10.8874 6.10653 11.1775 5.88273 11.559C5.65894 11.9405 5.57721 12.3889 5.65201 12.8248C5.7268 13.2608 5.9533 13.6562 6.29147 13.9413C6.62963 14.2264 7.0577 14.3828 7.5 14.3828C7.94231 14.3828 8.37037 14.2264 8.70854 13.9413C9.04671 13.6562 9.27321 13.2608 9.348 12.8248C9.4228 12.3889 9.34107 11.9405 9.11727 11.559C8.89348 11.1775 8.54201 10.8874 8.125 10.74V8.12499H11.25C11.5815 8.12499 11.8995 7.99329 12.1339 7.75887C12.3683 7.52445 12.5 7.20651 12.5 6.87499V4.88499C12.8648 4.75689 13.1809 4.51885 13.4047 4.20365C13.6286 3.88845 13.7492 3.51159 13.75 3.12498Z" />
+</svg>
+
+
