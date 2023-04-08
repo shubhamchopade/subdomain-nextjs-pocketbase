b3af40b5ffa7314113b4a2a1bb6c25f37e0c191f
shubham <shubhamchopade10@gmail.com>
2023-04-08 15:54:26 -0400

    implement global loader progress bar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -2,11 +2,17 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { useStore } from "../../store/store";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Navbar = () => {
   const session = useSession();
-  const isLoggedOut = session.data == null || session.data == undefined
-  const [userData, setUserData] = useState({})
+  const isLoggedOut = session.data == null || session.data == undefined;
+  const [userData, setUserData] = useState({});
+  const [loading, progress] = useStore((state) => [
+    state.loading,
+    state.progress,
+  ]);
 
   const handleSignin = async () => {
     signIn("github", { redirect: true, callbackUrl: "/" });
@@ -16,52 +22,86 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    setUserData(session.data?.user)
-  }, [session])
+    setUserData(session.data?.user);
+  }, [session]);
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">TechSapien</a>
-      </div>
-      {session.data && (
-        <div className="flex">
-          <Link className={`btn btn-primary mx-4 ${isLoggedOut && "hidden"}`} href={"/projects/create"}>
-            Create new project
-          </Link>
-          <Link className="link" href={"/projects"}>
-            Projects
-          </Link>
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">TechSapien</a>
         </div>
-      )}
-      <div className="flex-none">
-
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <Image width={50} height={50} className='rounded-full' src={userData?.picture || "https://avatar.vercel.sh/shubham.svg"} alt="profilepic" />
-            </div>
-          </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-              <p className="text-sm">{userData?.name}</p>
-            </li>
-            <li>
-              {session.data ? (
-                <button className="btn" onClick={handleSignOut}>
-                  SIGN OUT
-                </button>
-              ) : (
-                <button className="btn" onClick={handleSignin}>
-                  SIGN IN
-                </button>
-              )}
-            </li>
-          </ul>
+        {session.data && (
+          <div className="flex">
+            <Link
+              className={`btn btn-primary btn-sm mx-4 ${
+                isLoggedOut && "hidden"
+              }`}
+              href={"/projects/create"}
+            >
+              Create new project
+            </Link>
+            <Link className="link" href={"/projects"}>
+              Projects
+            </Link>
+          </div>
+        )}
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <Image
+                  width={50}
+                  height={50}
+                  className="rounded-full"
+                  src={
+                    userData?.picture || "https://avatar.vercel.sh/shubham.svg"
+                  }
+                  alt="profilepic"
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <p className="text-sm">{userData?.name}</p>
+              </li>
+              <li>
+                {session.data ? (
+                  <button className="btn" onClick={handleSignOut}>
+                    SIGN OUT
+                  </button>
+                ) : (
+                  <button className="btn" onClick={handleSignin}>
+                    SIGN IN
+                  </button>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+      {loadingProgressBar(loading, progress)}
+    </>
   );
 };
 
 export default Navbar;
+
+function loadingProgressBar(loading: boolean, progress: number) {
+  return (
+    <AnimatePresence>
+      {loading && (
+        <motion.div
+          initial={{ width: 0, height: 2 }}
+          animate={{ width: progress + "%" }}
+          transition={{ duration: 0.5 }}
+          exit={{ opacity: 0, height: 0 }}
+          className="bg-secondary"
+        />
+      )}
+    </AnimatePresence>
+  );
+}
