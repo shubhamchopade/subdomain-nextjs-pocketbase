71d4295e24384e509c15eb3f7630f731baae34e7
shubham <shubhamchopade10@gmail.com>
2023-04-16 15:36:58 -0400

    misc

--- backend/node-multithreading.ts
+++ backend/node-multithreading.ts
@@ -1,46 +1,32 @@
 import { spawn } from "child_process";
 
 export function executeCommandChild(command: string, args: any) {
-    return new Promise((resolve, reject) => {
-        const childProcess = spawn(command, args, { stdio: "pipe", shell: true });
+  return new Promise((resolve, reject) => {
+    const childProcess = spawn(command, args, { stdio: "pipe", shell: true });
 
-        let stdout = "";
-        let stderr = "";
+    let stdout = "";
+    let stderr = "";
 
-        childProcess.stdout.on("data", (data) => {
-            stdout += data.toString();
-            process.stdout.write(data);
-        });
-
-        childProcess.stderr.on("data", (data) => {
-            stderr += data.toString();
-            process.stderr.write(data);
-        });
-
-        childProcess.on("error", (err) => {
-            reject({ err, stderr });
-            // reject(
-            //     new Error(
-            //         `Command executeCommandChild "${command} ${args.join(
-            //             " "
-            //         )}" failed with error: ${err}`
-            //     )
-            // );
-        });
-
-        childProcess.on("close", (code, signal) => {
-            if (code !== 0) {
-                reject({ stderr });
-                // reject(
-                //     new Error(
-                //         `Command "${command} ${args.join(
-                //             " "
-                //         )}" failed with code ${code} and signal ${signal}: ${stderr}`
-                //     )
-                // );
-            } else {
-                resolve({ stdout, stderr });
-            }
-        });
+    childProcess.stdout.on("data", (data) => {
+      stdout += data.toString();
+      process.stdout.write(data);
     });
+
+    childProcess.stderr.on("data", (data) => {
+      stderr += data.toString();
+      process.stderr.write(data);
+    });
+
+    childProcess.on("error", (err) => {
+      reject({ err, stderr });
+    });
+
+    childProcess.on("close", (code, signal) => {
+      if (code !== 0) {
+        reject({ stderr });
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
 }
